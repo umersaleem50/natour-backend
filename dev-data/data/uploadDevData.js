0ee0d4ff934e2721a63ca6c38ed8e4edfa1202eb
umersaleem50 <umersaleem50@gmail.com>
2022-03-13 14:20:24 +0500

    Dev data upload to db

---
+++ dev-data/data/uploadDevData.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: `${__dirname}/../../config.env` });
+
+const DB = process.env.DB_URL.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log(`Database connected sucessfully!`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const toursData = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(toursData);
+    console.log(`Data imported sucessfully!`);
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log(`Data deleted sucessfully!`);
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+}
+if (process.argv[2] === '--delete') {
+  deleteData();
+}
